4c14c8039f5916d98984c0f65fb985662002443d
Ренат <syltan.100renrgen@gmail.com>
2022-02-26 22:50:59 +0300

    add spawner

---
+++ Assets/Scripts/Enemy/Pool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Pool : MonoBehaviour
+{
+    [SerializeField] private int _capacity;
+
+    [SerializeField] protected Transform Container;
+
+    private List<GameObject> _pool = new List<GameObject>();
+
+    protected void Init(List<GameObject> templates)
+    {
+        for(int i = 0; i < _capacity; i++)
+        {
+            var newObject = Instantiate(templates[Random.Range(0, templates.Count)], Container);
+            newObject.SetActive(false);
+            _pool.Add(newObject);
+        }       
+    }
+
+    protected bool TryGetObject(out GameObject gameObject)
+    {
+        gameObject = _pool.FirstOrDefault(o => o.activeSelf == false);
+
+        return gameObject != null;
+    }
+
+    protected void DisableObject()
+    {
+        Vector3 disablePoint = Camera.main.ViewportToWorldPoint(new Vector2 (-0.5f, 0));
+
+        foreach (var item in _pool)
+        {
+            if (item.activeSelf == true)
+            {
+                if (item.transform.position.x <= disablePoint.x)
+                {
+                    item.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : Pool
+{
+    [SerializeField] private List<GameObject> _templates;
+    [SerializeField] private float _delay;
+    [SerializeField] private float _minSpawnPoint;
+    [SerializeField] private float _maxSpawnPoint;
+
+    private float _elepsedTime;
+
+    private void Start()
+    {
+        Init(_templates);
+    }
+
+    private void Update()
+    {
+        _elepsedTime += Time.deltaTime;
+
+        if (_elepsedTime >= _delay)
+        {
+            if (TryGetObject(out GameObject obj))
+            {
+                Spawn(obj);
+            }
+
+            _elepsedTime = 0;
+        }
+    }
+
+    private void Spawn(GameObject obj)
+    {
+        obj.SetActive(true);
+        obj.transform.position = new Vector2(Container.position.x, Container.position.y + Random.Range(_minSpawnPoint, _maxSpawnPoint));
+        DisableObject();
+    }
+}
