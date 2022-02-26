ccca63276044c7c225a07ec6891cce0cbe1d23b5
Ренат <syltan.100renrgen@gmail.com>
2022-02-26 21:49:08 +0300

    add player

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth;
+
+    private int _currentHealth;
+
+    public event UnityAction Dying;
+    public event UnityAction ChangeHealth;
+
+    public int MaxHealth => _maxHealth;
+    public int CurrentHealth => _currentHealth;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Meteor meteor))
+        {
+            _currentHealth -= meteor.Damage;
+            ChangeHealth?.Invoke();
+
+            if (_currentHealth <= 0)
+            {
+                Dying?.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speedMove;
+    [SerializeField] private float _minDistanceX;
+    [SerializeField] private float _maxDistanceX;
+    [SerializeField] private float _minDistanceY;
+    [SerializeField] private float _maxDistanceY;
+    private void Update()
+    {
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);          
+        transform.position = Vector2.Lerp(transform.position, objPosition, _speedMove * Time.deltaTime);
+        float correctPositionX = Mathf.Clamp(transform.position.x, _minDistanceX, _maxDistanceX);
+        float correctPositionY = Mathf.Clamp(transform.position.y, _minDistanceY, _maxDistanceY);
+        transform.position = new Vector2(correctPositionX, correctPositionY);
+    }
+}
