03856183ff6405f8d785ea5853673bac56fd4623
Ренат <syltan.100renrgen@gmail.com>
2022-02-27 23:45:23 +0300

    update move player

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -9,12 +9,28 @@ public class PlayerMover : MonoBehaviour
     [SerializeField] private float _maxDistanceX;
     [SerializeField] private float _minDistanceY;
     [SerializeField] private float _maxDistanceY;
+
+    private Vector3 newTargetPosition;
+    private Vector3 oldPosition;
+
     private void Update()
     {
-        Vector2 objPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);          
-        transform.position = Vector2.Lerp(transform.position, objPosition, _speedMove * Time.deltaTime);
-        float correctPositionX = Mathf.Clamp(transform.position.x, _minDistanceX, _maxDistanceX);
-        float correctPositionY = Mathf.Clamp(transform.position.y, _minDistanceY, _maxDistanceY);
-        transform.position = new Vector2(correctPositionX, correctPositionY);
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                newTargetPosition = mousePosition;
+                oldPosition = transform.position;
+
+                Debug.Log(1);
+            }
+
+            transform.position = Vector2.Lerp(transform.position, oldPosition - (newTargetPosition - mousePosition), _speedMove * Time.deltaTime); ;
+            float correctPositionX = Mathf.Clamp(transform.position.x, _minDistanceX, _maxDistanceX);
+            float correctPositionY = Mathf.Clamp(transform.position.y, _minDistanceY, _maxDistanceY);
+            transform.position = new Vector2(correctPositionX, correctPositionY);
+        }
     }
 }
