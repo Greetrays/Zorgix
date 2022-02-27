9905546a7932cf11013329822253a831c6400f7b
Ренат <syltan.100renrgen@gmail.com>
2022-02-27 13:14:34 +0300

    update health bar

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -7,6 +7,9 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private PlayerHealth _player;
     [SerializeField] private Slider _bar;
+    [SerializeField] private float _speedChange;
+
+    private Coroutine _changeHealth;
 
     private void OnEnable()
     {
@@ -22,10 +25,28 @@ public class HealthBar : MonoBehaviour
     {
         _bar.maxValue = _player.MaxHealth;
         _bar.value = _player.MaxHealth;
+        _changeHealth = null;
     }
 
     private void OnChangeHealth()
     {
-        _bar.value = _player.CurrentHealth;
+        if (_changeHealth != null)
+        {
+            StopCoroutine(_changeHealth);
+        }
+
+        _changeHealth = StartCoroutine(ChangeHealth());
+    }
+
+    private IEnumerator ChangeHealth()
+    {
+        while(_bar.value > _player.CurrentHealth)
+        {
+            _bar.value = Mathf.MoveTowards(_bar.value, _player.CurrentHealth, _speedChange * Time.deltaTime);
+
+            yield return null;
+        }
+
+        _changeHealth = null;
     }
 }
